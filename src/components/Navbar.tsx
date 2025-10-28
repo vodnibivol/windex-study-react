2a9e8cb878c14c1794e7c2429af80f17bc8b7a17
vodnibivol <vodnibivol@gmail.com>
2025-10-28 18:53:49 +0100

    added collapsible menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './Navbar.scss';
 
+import { IoCloseOutline, IoMenuOutline } from 'react-icons/io5';
+
 const Navbar = () => {
   const [shadow, setShadow] = useState(false);
   useEffect(() => {
@@ -13,6 +15,8 @@ const Navbar = () => {
     setShadow(window.scrollY > 0);
   }
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={shadow ? 'shadow' : ''}>
       <div className="nav-left">
@@ -20,23 +24,30 @@ const Navbar = () => {
           <img src="./logo.svg" alt="Windex logo" />
           <span>Windex</span>
         </div>
-        <div className="links">
-          <a href="/home" className="underline-hover">
-            Home
-          </a>
-          <a href="/about" className="underline-hover">
-            About
-          </a>
-          <a href="/solutions" className="underline-hover">
-            Solutions
-          </a>
-          <a href="/projects" className="underline-hover">
-            Projects
-          </a>
-        </div>
+        {menuOpen && (
+          <div className="links">
+            <a href="/home" className="underline-hover">
+              Home
+            </a>
+            <a href="/about" className="underline-hover">
+              About
+            </a>
+            <a href="/solutions" className="underline-hover">
+              Solutions
+            </a>
+            <a href="/projects" className="underline-hover">
+              Projects
+            </a>
+          </div>
+        )}
       </div>
       <div className="nav-right">
-        <button type="button">Contact Us</button>
+        <button type="button" className="contact-button">
+          Contact Us
+        </button>
+        <button type="button" className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <IoCloseOutline /> : <IoMenuOutline />}
+        </button>
       </div>
     </nav>
   );
