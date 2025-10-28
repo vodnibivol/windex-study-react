83528ef37465bf7293c9f5515cfa39d7f54d8f43
vodnibivol <vodnibivol@gmail.com>
2025-10-28 11:12:10 +0100

    removed dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/windex-study-react/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
